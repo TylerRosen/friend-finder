96c3add81798eeb218084b8c095c70217d18f61e
TylerRosen <tylermrosen@gmail.com>
2017-02-27 15:44:12 -0500

    Added route paths

---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+// ===========================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Routes
+// =============================================================
+
+// Get routes
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+  });
