eb606cf6889d8f6514a5659807b88348945f2431
Tyler Rosen <tylermrosen@gmail.com>
2017-05-25 13:49:15 -0400

    CSS now displays

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,11 +2,11 @@ var path = require("path");
 
 function routes(app) {
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname + "/../survey.html"));
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
 
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname + "/../home.html"));
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 };
 
