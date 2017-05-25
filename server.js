95f30c2407cb131bb7a2b3c5882a1efd35ac953f
Tyler Rosen <tylermrosen@gmail.com>
2017-05-25 13:07:37 -0400

    Moved public folder

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text({type: 'text/html'}));
 app.use(bodyParser.json({ type: 'application/*+json'}));
